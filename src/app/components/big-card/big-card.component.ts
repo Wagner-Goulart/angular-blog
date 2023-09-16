fc4898b14b62066e9dd98cebfec48b4f9a1fc264
Wagner <wagnergoulart0@gmail.com>
2023-09-16 16:36:24 -0300

    ADD: propriedades aos componentes

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.css'],
 })
 export class BigCardComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  photoCover: string ="";
+  @Input()
+  cardTitle: string = '';
+  @Input()
+  cardDescription: string = '';
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
