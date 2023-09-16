e11987f7810ede1a43cf98162fdffe591981772e
Wagner <wagnergoulart0@gmail.com>
2023-09-16 18:17:57 -0300

    finalização

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+  {
+    "id": "1",
+    'title' : "Saiu filme novo",
+    "description": "Homendo de Ferro",
+    'photoCover': 'https://www.einerd.com.br/wp-content/uploads/2017/07/robert-downey-jr-homem-de-ferro-890x466.jpg'
+  },
+  {
+    "id": "2",
+    'title' : "Série Nova",
+    "description": "Homem de ferro levando ferro",
+    'photoCover': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPO5tyQ7YIDsE37vATYqzN9lcoTM4A2USXUA&usqp=CAU'
+  },
+
+]
